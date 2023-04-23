b7cb26cb85a0243662d815a05dd4852bacde0a83
ToirovH <131583343+ToirovH@users.noreply.github.com>
2023-04-23 14:31:11 +0500

    Create main.py
    version 1.0

---
+++ main.py
@@ -0,0 +1,11 @@
+window = display.set_mode((700,500))
+background = transform.scale(image.load('background2.jpg'),(700,500))
+clock = time.Clock()
+game = True
+while game:
+    window.blit(background,(0,0))
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    clock.tick(60)
+    display.update
